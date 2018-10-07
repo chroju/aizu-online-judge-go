739745599813b73a58461c956c2c72af4327afdb
chroju <chor.chroju@gmail.com>
2018-10-07 09:46:45 +0900

    ALDS1_1_A

---
+++ ALDS1_1_A/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Scan()
+	num, _ := strconv.Atoi(sc.Text())
+	sc.Scan()
+	inputs := strings.Split(sc.Text(), " ")
+
+	var raw = make([]int, num)
+	for i := range raw {
+		raw[i], _ = strconv.Atoi(inputs[i])
+	}
+
+	for i := range raw {
+		key := raw[i]
+		for j := i - 1; j >= 0; j-- {
+			if raw[j] > key {
+				raw[j+1] = raw[j]
+				if j == 0 {
+					raw[0] = key
+				}
+			} else {
+				raw[j+1] = key
+				break
+			}
+		}
+		fmt.Println(strings.Trim(strings.Join(strings.Fields(fmt.Sprint(raw)), " "), "[]"))
+	}
+
+}
