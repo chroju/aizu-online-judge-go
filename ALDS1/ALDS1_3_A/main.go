ef7b43a13faf4020bd630d48fa94a4d41ab6ce9e
chroju <chor.chroju@gmail.com>
2018-10-27 14:12:13 +0900

    ALDS1_3_A

---
+++ ALDS1/ALDS1_3_A/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+
+	var stack []string
+	for sc.Scan() {
+		fmt.Println(stack)
+		i := sc.Text()
+		if i == "+" || i == "-" || i == "*" {
+			a, _ := strconv.Atoi(stack[len(stack)-2])
+			b, _ := strconv.Atoi(stack[len(stack)-1])
+			if len(stack) > 2 {
+				stack = stack[:len(stack)-2]
+			} else {
+				stack = []string{}
+			}
+			if i == "+" {
+				stack = append(stack, strconv.Itoa(a+b))
+			} else if i == "-" {
+				stack = append(stack, strconv.Itoa(a-b))
+			} else if i == "*" {
+				stack = append(stack, strconv.Itoa(a*b))
+			}
+		} else {
+			stack = append(stack, i)
+		}
+	}
+	fmt.Println(stack[0])
+}
